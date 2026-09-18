839305a00f537d0fd92981814c5427ee2be7d716
Agbomi5 <agbomi5@users.noreply.github.com>
2026-09-18 23:57:39 +0100

    Add PostgreSQL support for Vercel deployment

--- onlinebankingportalproject/settings.py
+++ onlinebankingportalproject/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +74,7 @@ WSGI_APPLICATION = 'onlinebankingportalproject.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
